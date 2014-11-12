4dbea88cce895ab7f50fa86b8299ab1887bc9c31
Max Dzitsiuk <max.dzitsiuk@gmail.com>
2014-11-12 14:32:37 +0100

    Added prototype assets. Camera tweened in demo scene.

---
+++ Assets/LineToObject.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LineToObject : MonoBehaviour {
+    // Populate this array in the order you want the line to travel. 
+    public Transform transform;
+    public Material mat;
+
+    private LineRenderer lineRenderer;
+
+	void Start () {
+	    lineRenderer = gameObject.AddComponent<LineRenderer>();
+        lineRenderer.SetColors(Color.red, Color.red);
+        lineRenderer.material = mat;
+        lineRenderer.SetWidth(0.1f, 0.1f);
+        lineRenderer.SetVertexCount(2);
+	}
+
+    void Update()
+    {
+        lineRenderer.SetPosition(0, gameObject.transform.position);
+        lineRenderer.SetPosition(1, transform.position);
+	}
+}
---
+++ Assets/TweenObjectToTransforms.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TweenObjectToTransforms : MonoBehaviour {
+
+    public Transform target;
+    public Transform[] transforms;
+
+    private int currentTransformIndex = 0;
+
+	private bool isTweening = false;
+
+	void Start()
+	{
+        if (transforms.Length > 0)
+        {
+			target.position = transforms[0].position;
+			currentTransformIndex = 0;
+        }
+	}
+	
+	void Update()
+	{
+		bool moveToNext = Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.RightArrow);
+		bool moveToPrev = Input.GetKeyDown(KeyCode.Backspace) || Input.GetKeyDown(KeyCode.LeftArrow);
+		if (moveToNext || moveToPrev)
+		{
+			if (moveToNext && currentTransformIndex < transforms.Length - 1)
+			{
+				currentTransformIndex++;
+				isTweening = true;
+			}
+
+			if (moveToPrev && currentTransformIndex > 0)
+			{
+				currentTransformIndex--;
+				isTweening = true;
+			}
+		}
+
+		if (isTweening)
+		{
+			target.position = Vector3.Lerp(target.position, transforms[currentTransformIndex].position, Time.deltaTime * 2);
+			if (Vector3.Distance(target.position, transforms[currentTransformIndex].position) < 0.05)
+			{
+				isTweening = false;
+				target.position = transforms[currentTransformIndex].position;
+			}
+		}
+	}
+
+}
